859c72c545b3450b3727b80a388b3a9f871f6c7c
Rachel Hwang <rah1@uchicago.edu>
2015-10-11 19:42:14 -0400

    Ray tracing working, implementing BVH, wip

--- src/src/scene/geometry/boundingbox.h
+++ src/src/scene/geometry/boundingbox.h
@@ -1,43 +1,5 @@
-#pragma once
+#ifndef BOUNDINGBOX_H
+#define BOUNDINGBOX_H
 
-#include <openGL/drawable.h>
-#include <scene/geometry/geometry.h>
+#endif // BOUNDINGBOX_H
 
-class Geometry;
-class BoundingBox : public Drawable
-{
-public:
-    BoundingBox():
-    minimum(), maximum() {
-        object = NULL;
-    }
-
-    ~BoundingBox(){}
-    void create();
-    GLenum drawMode();
-    glm::vec3 GetCenter();
-    static BoundingBox Union(const BoundingBox &a, const BoundingBox &b);
-
-    glm::vec3 center;
-    glm::vec3 minimum;
-    glm::vec3 maximum;
-    Geometry *object;
-};
-
-class bvhNode
-{
-public:
-    bvhNode():
-    bounding_box() {
-        left = NULL;
-        right = NULL;
-    }
-
-    static bvhNode *CreateTree(std::vector<bvhNode> &leaves, int depth, int start_idx, int end_idx);
-    static bvhNode *InitTree(QList<Geometry*> objects);
-
-    BoundingBox bounding_box;
-    bvhNode *left;
-    bvhNode *right;
-    int dimension;
-};
