923748365510ad3d14afadf9670be84c1fc7f984
Rachel Hwang <rah@Rachels-MacBook-Pro.local>
2015-10-25 18:10:00 -0400

    Adding hw4 path tracer code

---
+++ path_tracer/src/raytracing/integrator.cpp
@@ -0,0 +1,19 @@
+#include <raytracing/integrator.h>
+
+
+Integrator::Integrator():
+    max_depth(5)
+{
+    scene = NULL;
+    intersection_engine = NULL;
+}
+
+glm::vec3 ComponentMult(const glm::vec3 &a, const glm::vec3 &b)
+{
+    return glm::vec3(a.x * b.x, a.y * b.y, a.z * b.z);
+}
+
+void Integrator::SetDepth(unsigned int depth)
+{
+    max_depth = depth;
+}
---
+++ path_tracer/src/scene/geometry/geometry.cpp
@@ -0,0 +1,13 @@
+#include <scene/geometry/geometry.h>
+
+float Geometry::RayPDF(const Intersection &isx, const Ray &ray)
+{
+    //TODO
+    //The isx passed in was tested ONLY against us (no other scene objects), so we test if NULL
+    //rather than if != this.
+    if(isx.object_hit == NULL)
+    {
+        return 0;
+    }
+    //Add more here
+}
