affd6b93e67bb263638a19e972aa351ef71939a8
Trung Le <trungle@seas.upenn.edu>
2015-11-25 03:20:13 -0500

    [bxdf] - Update specular

--- path_tracer/src/scene/materials/bxdfs/specularreflectionbxdf.cpp
+++ path_tracer/src/scene/materials/bxdfs/specularreflectionbxdf.cpp
@@ -14,15 +14,26 @@ glm::vec3 SpecularReflectionBxDF::EvaluateHemisphereScatteredEnergy(const glm::v
 
 glm::vec3 SpecularReflectionBxDF::SampleAndEvaluateScatteredEnergy(const glm::vec3 &wo, glm::vec3 &wi_ret, float rand1, float rand2, float &pdf_ret) const
 {
+    // Populate return values
+    // Since we're in the shading coordinates, the surface normal is 0,0,1
+    // and the reflection of wo is just pi radians about the normal
     wi_ret = glm::vec3(-wo.x, -wo.y, wo.z);
-    pdf_ret = 1;
-    float cosi = glm::dot(wo, glm::normalize(wo + wi_ret));
-    return reflection_color * FresnelTerm(cosi) / glm::abs(wi_ret.z);
+    pdf_ret = PDF(wo, wi_ret); // direct delta, pdf = 1.f with the reflected ray and 0.f everywhere else
+
+    return EvaluateScatteredEnergy(wo, wi_ret);
 }
 
 float SpecularReflectionBxDF::PDF(const glm::vec3 &wo, const glm::vec3 &wi) const {
-    if (fequal(-wo.x, wi.x), fequal(-wo.y, wi.y), fequal(wo.z, wo.z)) {
-        return 1;
+
+    // Perfect specular only returns the color in the direction of the reflection
+    // Since we're in the shading coordinates, the surface normal is 0,0,1
+    // and the reflection of wo is just pi radians about the normal
+
+    float cos_theta_reflected = glm::abs(wo.z);
+    float cos_theta_wi = glm::abs(wi.z);
+    if (fequal(float(cos_theta_reflected - cos_theta_wi), 0.f)) {
+        return 1.f;
+    } else {
+        return 0.f;
     }
-    return 0;
 }
