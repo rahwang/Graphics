01f21367b1d5b8b19d653f6ada227f6a5846254a
Trung Le <trungle@seas.upenn.edu>
2015-12-06 15:18:25 -0500

    [photon] - Created copy constructor and overload assignment operator for class Photon

--- path_tracer/src/raytracing/photon.h
+++ path_tracer/src/raytracing/photon.h
@@ -5,13 +5,32 @@
 class Photon
 {
 public:
+    Photon() :
+        position(), wi(), color()
+    {
+
+    }
+
     Photon(const glm::vec3& position, const glm::vec3& wi, const glm::vec3& color) :
         position(position), wi(wi), color(color)
     {
 
     }
 
+    Photon(const Photon& ph):
+        position(ph.position), wi(ph.wi), color(ph.color)
+    {
+    }
+
+    Photon& operator=(const Photon& ph)
+    {
+        position = ph.position;
+        wi = ph.wi;
+        color = ph.color;
+    }
+
     glm::vec3 position;
     glm::vec3 wi;
     glm::vec3 color;
 };
+
